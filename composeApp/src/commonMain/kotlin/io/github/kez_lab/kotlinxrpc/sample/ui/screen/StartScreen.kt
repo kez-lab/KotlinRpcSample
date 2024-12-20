1762865a523b52a6215468c506443b369ffe4893
KEZ <yuijin0909@gmail.com>
2024-12-20 16:06:48 +0900

    remove non used import

--- composeApp/src/commonMain/kotlin/io/github/kez_lab/kotlinxrpc/sample/ui/screen/StartScreen.kt
+++ composeApp/src/commonMain/kotlin/io/github/kez_lab/kotlinxrpc/sample/ui/screen/StartScreen.kt
@@ -3,7 +3,6 @@ package io.github.kez_lab.kotlinxrpc.sample.ui.screen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Button
